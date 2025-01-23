a4fbc1a5382fc01640cb677c61b368b1ff60632f
Antonia <119491435+AntoniaDnv@users.noreply.github.com>
2025-01-23 12:29:32 +0200

    js exercises

---
+++ dom-lab-03.4/DeleteFromTable/app.js
@@ -0,0 +1,12 @@
+function deleteByEmail() {
+    let input = document.querySelector('input');
+    let emails = document.querySelectorAll('tr td:nth-child(2)');
+    let table = document.querySelector('table');
+    array.forEach(el => {
+        if(el.textContent == input.value){
+            let curEl = el.parentElement;
+            table.removeChild(curEl);
+        }
+        
+    });
+}
